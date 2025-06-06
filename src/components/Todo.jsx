c6cf07261ef3258d78e7648c86ef2923ebc7842f
Mithun-74 <mithun.coder22@gmail.com>
2025-06-06 15:32:17 +0530

    updated some changes

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,17 +5,24 @@ import { Todoitem } from './Todoitem';
 let count = 0;
 export const Todo = () => {
    
-   const[todos,setTodos] = useState([]);
+   const[todos,setTodos] = useState(() => JSON.parse(localStorage.getItem("todos") || "[]"));
    const inputRef = useRef(null);
 
    const add=()=>{
-          setTodos([...todos,{no:count++,text:inputRef.current.value,display:""}]);
+          setTodos([...todos, {no:count++,text:inputRef.current.value,display:""}]);
           inputRef.current.value = "";
+          localStorage.setItem("todos_count",count);
    }
 
-   useEffect(()=>{
-       console.log(todos);
-   },[todos])
+ useEffect(()=>{
+  const savedCount = localStorage.getItem("todos_count");
+  if (savedCount) count = parseInt(savedCount);
+},[])
+
+   useEffect(() => {
+        console.log(todos);
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
 
   return (
     <div className='todo'>
@@ -26,7 +33,7 @@ export const Todo = () => {
         </div>
         <div className="todo-list">
             {todos.map((item,index)=>{
-              return <Todoitem key={index} no={item.no} display={item.display} text={item.text} />
+              return <Todoitem key={index} setTodos={setTodos} no={item.no} display={item.display} text={item.text} />
             })}
         </div>
     </div>
--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -2,15 +2,39 @@ import React from 'react'
 import './CSS/Todoitem.css'
 import  tick from './Assets/tick.png'
 import wrong from './Assets/wrong.png'
+import not_tick from './Assets/not tick.png'
 
-export const Todoitem = ({no,display,text}) => {
+export const Todoitem = ({no,display,text,setTodos}) => {
+
+
+  const deleteTodo = (no)=>{
+    let data = JSON.parse(localStorage.getItem("todos"));
+    data = data.filter((todos)=> todos.no!==no);
+    setTodos(data);
+  }
+
+  const toogle = ()=>{
+       let data = JSON.parse(localStorage.getItem("todos"));
+       for(let i=0;i<data.length;i++){
+        if(data[i].no===no){
+          if(data[i].display===""){
+             data[i].display = "line-through";
+          }
+          else{
+            data[i].display = "";
+          }
+          break;
+        }
+       }
+      setTodos(data);
+  }
   return (
     <div className='todoitems'>
-      <div className="todo-item-container">
-        <img src={wrong} alt="" />
+      <div className="todo-item-container" onClick={()=>{toogle(no)}}>
+        {display===""? <img src={not_tick} alt="" />:<img src={tick} alt="" />}
         <div className="todoitems-text">{text}</div>
       </div>
-        <img src={tick} alt="" />
+        <img className='todo-cross-icon' onClick={()=>{deleteTodo(no)}} src={wrong} alt="" />
     </div>
   )
 }
